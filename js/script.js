8a21c6770c00f90217c2227b2632b4765c834f2b
Davi Costa <davifncosta@gmail.com>
2023-05-16 16:18:02 -0300

    organize files

---
+++ js/script.js
@@ -0,0 +1,37 @@
+"use strict";
+// Modal window
+import {
+  modal,
+  overlay,
+  btnCloseModal,
+  btnsOpenModal,
+  btnScrollTo,
+  section1,
+} from "./variables.js";
+
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+btnsOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
+
+btnScrollTo.addEventListener("click", (e) => {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+});
---
+++ js/variables.js
@@ -0,0 +1,10 @@
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+
+const btnCloseModal = document.querySelector(".btn--close-modal");
+const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+
+const section1 = document.querySelector("#section--1");
+
+export { modal, overlay, btnCloseModal, btnsOpenModal, btnScrollTo, section1 };
